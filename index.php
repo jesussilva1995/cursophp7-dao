edc982367934101aa9171a65cfb1f3c140d1492f
Jesus Silva <jesus-arturo@hotmail.com>
2018-09-13 17:57:43 +0100

    metodos de lista

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 
 	require_once("config.php");
 
+	//Consulta directa pela classe SQL
 	//$sql = new Sql();
 	//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	//echo json_encode($usuarios);
 
-
+	//Carrega um usuario
 	//$root = new Usuario();
 	//$root->loadbyId(3);
 	//echo $root;
+
+	//Carrega uma lista de usuarios
+	//$lista = Usuario::getList(); /*Maneira de chamar metodos estaticos*/
+	//echo json_encode($lista);
+
+
+	//Carrega uma lista de usuários buscando pelo login
+	//$search = Usuario::search("jo");
+	//echo json_encode($search);
+
+	//Carrega um usuário usando o login e a senha
+	//$usuario = new Usuario();
+	//$usuario->login("sqlserver","alb2c3");
+	//echo $usuario;
+
+
 
  ?>
